eee1799da016892c07b15d2bdc0ef64d50328c3b
Joffrey Chevalier <chevalier.joffrey@live.fr>
2022-03-18 15:24:11 +0100

    modif sur rse.html et script.js

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,6 @@
 const elemTimer = document.querySelector('#timer');
 let masqueJeter = 0;
 
-new Intl.NumberFormat('fr-FR').format(3141596);
-
 function comptMask() {
     masqueJeter += 39351;
     elemTimer.textContent = Math.floor(masqueJeter);
