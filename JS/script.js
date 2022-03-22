85664b14586f6cde77435137d36051bc625e9565
Joffrey Chevalier <chevalier.joffrey@live.fr>
2022-03-22 10:53:22 +0100

    try to add JS function for timer

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,13 @@
 const elemTimer = document.querySelector('#timer');
+
 let masqueJeter = 0;
+let masqueJeterPerSec = 39351;
+let masqueJeterFormated = new Intl.NumberFormat(undefined).format(masqueJeter)
+let masqueJeterPerSecFormated = new Intl.NumberFormat(undefined).format(masqueJeterPerSec)
 
 function comptMask() {
-    masqueJeter += 39351;
-    elemTimer.textContent = Math.floor(masqueJeter);
+    masqueJeterFormated += masqueJeterPerSecFormated;
+    elemTimer.textContent = Math.floor(masqueJeterFormated);
 }
 
 setInterval(comptMask, 1000 );
