8001537c762c5361ee6511f2c9b2af537edce687
THEO MICALETTI <theo.micaletti@icloud.com>
2022-03-23 15:10:01 +0100

    changement nav et index

--- JS/cssslidy.js
+++ JS/cssslidy.js
@@ -1,5 +1,5 @@
 let i = 0;
-let images = [src ="https://picsum.photos/600/300?random=1", "https://picsum.photos/600/300?random=2", "https://picsum.photos/600/300?random=3", "https://picsum.photos/600/300?random=4"];
+let images = [src ="https://i.postimg.cc/brT8xtPf/photo-1.jpg", "https://i.postimg.cc/1RHZYwkL/photo-2.jpg", "https://i.postimg.cc/9M1CT9C5/photo-3.jpg", "https://i.postimg.cc/TYK5ddJY/photo-4.jpg", "https://i.postimg.cc/sfN1NFCF/photo-5.jpg", "https://i.postimg.cc/KjBzTzqn/photo-6.jpg", "https://i.postimg.cc/5Nm9WDZN/photo-7.jpg", "https://i.postimg.cc/yY64cxbs/photo-8.jpg", "https://i.postimg.cc/qRrZrZf4/photo-9.jpg", "https://i.postimg.cc/wB2DtWy6/photo-10.jpg", "https://i.postimg.cc/qvLLPccC/photo-11.jpg", "https://i.postimg.cc/3rFdCL3p/photo-13.jpg"];
 let time = 6000;
 
 function changeImg(){
