6314a91eceed15c74dcdb41b1b0de27dfe08bbbf
Joffrey Chevalier <chevalier.joffrey@live.fr>
2022-03-25 12:57:26 +0100

    MAJ Footer et image index.html + JS RSE compteur avec virgule et image d'arriere plan

--- JS/script.js
+++ JS/script.js
@@ -6,19 +6,23 @@ let masqueJeter = 0;
 let masqueNature = 0;
 let masqueMplastique = 0;
 
+function format(number, options = {}) {
+    return new Intl.NumberFormat("fr-FR", options).format(number);
+}
+
 function comptMask() {
     masqueJeter += 0.039351;
-    maskTimer.textContent = Math.floor(masqueJeter);
+    maskTimer.textContent = format(masqueJeter, { minimumFractionDigits: 2,  maximumFractionDigits: 2});
 };
 
 function comptNature() {
     masqueNature += 197;
-    natureTimer.textContent = Math.floor(masqueNature);
+    natureTimer.textContent = format(Math.floor(masqueNature));
 };
 
 function comptMplastique() {
     masqueMplastique += 0.2955;
-    mPlasqtiqueTimer.textContent = Math.floor(masqueMplastique);
+    mPlasqtiqueTimer.textContent = format(masqueMplastique, { minimumFractionDigits: 2,  maximumFractionDigits: 2});
 };
 
 setInterval(comptMask, 1000 );
